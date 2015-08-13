a4e01c5aa7cf48bc66efc788466ca5cf0e94173a
Anthony Ecuman <aecuman24@gmail.com>
2015-08-13 22:54:07 +0300

    FirstCommit

---
+++ NavPet/Handler1.ashx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using JsonServices;
+using JsonServices.Web;
+
+namespace NavPet
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : JsonHandler
+    {
+
+        public Handler1()
+        {
+            this.service.Name = "Json";
+            this.service.Description = "JSON API for android application";
+            InterfaceConfiguration IConfig = new InterfaceConfiguration("RestAPI", typeof(IStationRepository), typeof(StationRepository));
+            this.service.Interfaces.Add(IConfig);
+        }
+    }
+}
